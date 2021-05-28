b659fb7b1a191e907206dc89be852647f0fe42c1
John Melody Me <Johnmelodyme@yandex.com>
2021-05-29 00:58:36 +0800

    Update luhn.erl

--- luhn.erl
+++ luhn.erl
@@ -2,8 +2,8 @@
 %% [project] : Luhn Algorithm 
 
 -module(luhn).
--import(string, [len/1, concat/2, chr/2, str/2, to_lower/1, to_upper/1]).
--export([check_luhn/1, start/1, log/1]).
+-import(string, [len/1, chr/2, str/2,  fac/1]).
+-export([check_luhn/1, for/2, start/1, log/1]).
 
 %% Luhn Algorithm, the modulus 10 or mod 10 algorithm, 
 %% is a simple checksum formula used to validate a variety 
@@ -15,8 +15,15 @@ check_luhn(CARD_NUMBER) ->
     Fn = fun() ->
         io:fwrite("Card Number: ~p~n", [CARD_NUMBER]),
         Len = len(CARD_NUMBER),
-        log(Len) end,
+        log(Len),
+        for(Len - 1, 1) end,
     Fn().
+
+for(0, _) ->
+    []; 
+    for (N, Term) when N >= 0 ->
+        io:fwrite("s"),
+        [Term|for(N-1, Term)].
 
 log(MSG) ->
     io:fwrite("~p~n", [MSG]).
